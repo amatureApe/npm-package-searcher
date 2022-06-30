9481e6aa0870fce9cfd14d36cb51fea16f379779
amatureApe <amatureApe@gmail.com>
2022-06-30 12:37:53 -0500

    setup API call

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -3,18 +3,20 @@ import { Dispatch } from 'redux';
 import { ActionType } from '../action-types';
 import { Action } from '../actions';
 
-const searchRepositories = (term: string) => {
+export const searchRepositories = (term: string) => {
   return async (dispatch: Dispatch<Action>) => {
     dispatch({
       type: ActionType.SEARCH_REPOSITORIES
     });
 
     try {
-      const { data } = await axios.get('https://registry.npmjs.org/-/v1/search', {
-        params: {
-          texts: term
+      const { data } = await axios.get('https://registry.npmjs.org/-/v1/search',
+        {
+          params: {
+            text: term
+          }
         }
-      });
+      );
 
       const names = data.objects.map((result: any) => {
         return result.package.name;
@@ -24,7 +26,6 @@ const searchRepositories = (term: string) => {
         type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
         payload: names
       });
-
     } catch (err: any) {
       dispatch({
         type: ActionType.SEARCH_REPOSITORIES_ERROR,
